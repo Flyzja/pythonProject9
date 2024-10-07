42660a4e4e5ec017547f85d9473e76d70fbf07d0
Flyzja <mamontova-75@mail.ru>
2024-10-07 21:07:33 +0300

    Введение в функциональное программирование

---
+++ module_9_1.py
@@ -0,0 +1,17 @@
+
+def apply_all_func(int_list = [6, 20, 15, 9], *functions):
+    dict = {}
+    for function in functions:
+        dict[function.__name__] = function(int_list)
+    return dict
+
+
+
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
+
+
+
+
+
